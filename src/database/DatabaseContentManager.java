9f1ddc1621b734b93926d62d90be98da55b5a7ef
VincentXis <rickard.bernal@gmail.com>
2016-12-06 19:39:44 +0100

    Server works, todo: set filename or file path as parameter, to enable multiple instances of server.

--- src/database/DatabaseContentManager.java
+++ src/database/DatabaseContentManager.java
@@ -1,19 +1,20 @@
 package database;
 
-import java.io.*;
-import java.net.Socket;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class DatabaseContentManager {
     private File contactData = new File("contact_data.csv");
 
-    public void readFromFile(PrintWriter out, Socket clientSocket) {
+    public void readFromFile(PrintWriter out) {
         String filePath = "src\\database\\";
         try (Scanner sc = new Scanner(new FileReader(filePath + contactData))) {
             while (sc.hasNextLine()) {
                 out.println(sc.nextLine().replaceAll(",", " "));
             }
-            clientSocket.shutdownOutput();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/server/Server.java
+++ src/server/Server.java
@@ -9,8 +9,8 @@ import java.util.Scanner;
 
 public class Server {
     private DatabaseContentManager dcm;
-    ServerSocket serverSocket;
-    Socket clientSocket;
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
 
     public Server() {
         dcm = new DatabaseContentManager();
@@ -34,6 +34,7 @@ public class Server {
             try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                  BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
                 clientRequestManagement(in, out);
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -45,23 +46,11 @@ public class Server {
         while ((clientInputRequest = in.readLine()) != null) {
             System.out.println("Request: " + clientInputRequest);
             if (clientInputRequest.equals("get all")) {
-
-                dcm.readFromFile(out, clientSocket);
-
-//                out.notify();
+                dcm.readFromFile(out);
+                out.println();
             } else if (clientInputRequest.equals("exit")) {
                 break;
             }
-        }
-    }
-
-    private void readFromFile(PrintWriter out) {
-        try (Scanner sc = new Scanner(new FileReader("src\\database\\contact_data.csv"))) {
-            while (sc.hasNextLine()) {
-                out.println(sc.nextLine().replaceAll(",", " "));
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
     }
 }
